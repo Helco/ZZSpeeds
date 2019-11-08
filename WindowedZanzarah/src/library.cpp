92ccf60fbbfe16f9016bcccd2056545c9625d749
Helco <hermann.noll@hotmail.com>
2019-11-08 14:38:54 +0100

    Center the game window in the nearest screen

--- WindowedZanzarah/src/library.cpp
+++ WindowedZanzarah/src/library.cpp
@@ -10,13 +10,37 @@ int __stdcall MyWndProc (HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	// which in modern windows systems is about anytime anything happens
 	// so we just call ClipCursor a lot, which is not pretty but works
 
-	if (msg == WM_ACTIVATE)
+	bool shouldRepositionWindow = false;
+
+	if (msg == WM_ACTIVATE) {
 		isWindowActivated = wParam > 0;
+		shouldRepositionWindow = isWindowActivated;
+	}
+
+	if (msg == WM_WINDOWPOSCHANGED)
+		shouldRepositionWindow = true;
+
 	if (isWindowActivated) {
 		RECT rect;
 		GetClientRect(hWnd, &rect);
 		MapWindowPoints(hWnd, HWND_DESKTOP, reinterpret_cast<POINT*>(&rect), 2);
 		ClipCursor(&rect);
+	}
+
+	if (shouldRepositionWindow) {
+		// the centering should not be too annoying, do it only if things change
+		auto monitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
+		MONITORINFO monitorInfo;
+		ZeroMemory(&monitorInfo, sizeof(MONITORINFO));
+		monitorInfo.cbSize = sizeof(MONITORINFO);
+		GetMonitorInfo(monitor, &monitorInfo);
+		RECT windowSize;
+		GetWindowRect(hWnd, &windowSize);
+		SetWindowPos(hWnd, HWND_TOP,
+			(monitorInfo.rcMonitor.left + monitorInfo.rcMonitor.right) / 2 - (windowSize.right - windowSize.left) / 2,
+			(monitorInfo.rcMonitor.top + monitorInfo.rcMonitor.bottom) / 2 - (windowSize.bottom - windowSize.top) / 2,
+			0, 0,
+			SWP_NOSIZE);
 	}
 
 	return originalWndProc(hWnd, msg, wParam, lParam);
